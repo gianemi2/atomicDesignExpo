758fb82d596ddf89161b61f7109925150745ee81
Marco Giannini <marco.giannini@6emme.it>
2020-02-06 22:51:02 +0100

    First release

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import RootNavigator from '_navigations'
+
+const App = () => {
+  return <RootNavigator />
+}
+
+export default App
